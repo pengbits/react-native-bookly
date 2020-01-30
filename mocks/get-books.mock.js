296b464ab5f7c665042d7b4a8bbec273f4c15e6e
Dave Paul <david.paul@showtime.net>
2020-01-30 16:03:47 -0500

    scraped some mock books together

--- mocks/get-books.mock.js
+++ mocks/get-books.mock.js
@@ -1,4 +1,64 @@
 export default {
   success: true,
-  books: []
+  books: [{
+      "title": "How to Behave in a Crowd",
+      "author": "Bordas, Camille"
+    }, {
+      "title": "The Family Fang",
+      "author": "Wilson, Kevin *"
+    }, {
+      "title": "Perfect Little World",
+      "author": "Wilson, Kevin *"
+    }, {
+      "title": "Nothing to See Here",
+      "author": "Wilson, Kevin *"
+    }, {
+      "title": "Little Fires Everywhere",
+      "author": "Ng, Celeste *"
+    }, {
+      "title": "Such a Fun Age",
+      "author": "Reid, Kiley"
+    }, {
+      "title": "Night Boat to Tangier",
+      "author": "Barry, Kevin"
+    }, {
+      "title": "Heroes of the Frontier",
+      "author": "Eggers, Dave"
+    }, {
+      "title": "Severance",
+      "author": "Ma, Ling *"
+    }, {
+      "title": "Half Blood Blues",
+      "author": "Edugyan, Esi"
+    }, {
+      "title": "A Brief History of Seven Killings",
+      "author": "James, Marlon"
+    }, {
+      "title": "Washington Black",
+      "author": "Edugyan, Esi"
+    }, {
+      "title": "The Answers",
+      "author": "Lacey, Catherine *"
+    }, {
+      "title": "Asymmetry",
+      "author": "Halliday, Lisa"
+    }, {
+      "title": "There There",
+      "author": "Orange, Tommy *"
+    }, {
+   		"title":"Lake Success",
+      "author": "Shteyngart, Gary *"
+    }, {
+      "title": "Little Failure",
+      "author": "Shteyngart, Gary *"
+    }, {
+      "title": "Dreams from My Father: A Story of Race and Inheritance",
+      "author": "Obama, Barack"
+    }, {
+      "title": "The Beautiful Struggle: A Father, Two Sons and an Unlikely Road to Manhood",
+      "author": "Coates, Ta-Nehisi"
+    }, {
+      "title": "We Were Eight Years in Power: An American Tragedy",
+      "author": "Coates, Ta-Nehisi"
+    }]
 }
