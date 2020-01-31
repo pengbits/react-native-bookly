bec5546918824f2b0a185e99f3513037963a2443
Dave Paul <david.paul@showtime.net>
2020-01-31 10:12:58 -0500

    cleanup

--- src/server/db/seeds.js
+++ src/server/db/seeds.js
@@ -4,6 +4,12 @@ import Book   from '../models/book.model'
 import GetAuthorsMock from '../../../mocks/get-authors.mock'
 import GetBooksMock   from '../../../mocks/get-books.mock'
 
+// mandatory naming-conventions in use:
+// key 'MyModel' - capitalized name
+// GetMyModelMock - mock request response
+// GetMyModelMock.mymodel - array of model attrs/payload
+// 
+
 const seedable_models = {
   Author,
   Book
@@ -14,49 +20,6 @@ const seedable_mocks = {
   'Book': GetBooksMock.books
 }
 
-export const SeedAuthors = () => {
-
-  console.log('deleting all authors from db...')
-  Author.deleteMany({}, function(res){
-    res && console.log(res)
-  })
-  console.log(`loading ${GetAuthorsMock.authors.length} authors into db...`)
-  const fixtures = GetAuthorsMock.authors.map((author) => {
-    // reject reserved attributes
-    const {_id,__v, ...attrs} = author
-    new Author(attrs).save()
-  })
-
-}
-
-export const SeedBooks = () => {
-  console.log('deleting all books from db...')
-  Book.deleteMany({}, function(res){
-    res && console.log(res)
-  })
-
-  console.log(`loading ${GetBooksMock.books.length} books into db...`)
-  const fixtures = GetBooksMock.books.map((author) => {
-    const {_id,__v, ...attrs} = author
-    return attrs
-  })
-  
-  Book.insertMany(fixtures, (err,docs) =>{
-    if(err) {
-      console.log(err)
-    } else {
-      console.log('success')
-    }
-  })
-
-}
-
-
-// naming-conventions in use
-// key 'MyModel' - capitalized name
-// GetMyModelMock - mock request response
-// GetMyModelMock.mymodel - array of model attrs/payload
-// 
 export const SeedModel = async (key) => {
   const model = seedable_models[key]
   if(!model) throw new Error('couldn\'t seed '+key)
