40b43d1b95f983b479e31e42ec00099e94601cd2
Dave Paul <david.paul@showtime.net>
2020-02-18 16:07:46 -0500

    starting over w/ newly generated app

--- android/app/src/main/java/com/libros/MainApplication.java
+++ android/app/src/main/java/com/libros/MainApplication.java
@@ -1,4 +1,4 @@
-package com.bookly;
+package com.libros;
 
 import android.app.Application;
 import android.content.Context;
--- ios/librosTests/librosTests.m
+++ ios/librosTests/librosTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface booklyTests : XCTestCase
+@interface librosTests : XCTestCase
 
 @end
 
-@implementation booklyTests
+@implementation librosTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
